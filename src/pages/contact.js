b0b723c4bdbcfe877a572a5a056f82b1356dbae3
Liz Tiller <lizthrilla@users.noreply.github.com>
2018-01-05 12:23:43 -0500

    tutorial 3 done

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => (
+    <div>
+        <h1>I'd love to talk! Email me at the address below</h1>
+        <p>
+            <a href="mailtome@example.com">me@example.com</a>
+        </p>
+    </div>
+)
